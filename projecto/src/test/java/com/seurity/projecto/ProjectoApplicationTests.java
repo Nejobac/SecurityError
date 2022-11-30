3b6e7a3013e3f400898e2f7d08761e855fe9d686
Nejobac <nejobac@gmail.com>
2022-11-30 17:24:03 -0300

    subi el error

---
+++ projecto/src/test/java/com/seurity/projecto/ProjectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seurity.projecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
